bc4d03e853d637de6ee65a2c1c69ed3844fb3635
Cezar Halmagean <cezar@mixandgo.com>
2022-06-20 09:47:38 +0300

    Generated posts controller

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,10 @@
+class PostController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts, only: %i[new index create]
+  root "post#index"
 end
