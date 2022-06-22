4c1cbe28db952fd61c1a6287da80c7d74fe5f640
Cezar Halmagean <cezar@mixandgo.com>
2022-06-22 08:40:12 +0300

    Added post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ApplicationRecord
+  has_one_attached :file
+  has_many :likes
+  has_many :comments
   belongs_to :user
+
+  validates :body, presence: true
+  validates :file, presence: true
+
+  after_create_commit do 
+    broadcast_prepend_to "posts_list", target: "posts", partial: "posts/post", locals: { post: self, user: self.user }
+  end
+
+  def like!(user)
+    likes << Like.new(user: user)
+  end
+
+  def unlike!(user)
+    likes.where(user_id: user.id).delete_all
+  end
+
+  def liked?(user)
+    likes.where(user_id: user.id).any?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :comments
+  has_many :likes
 end
