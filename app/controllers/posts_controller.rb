51ffb1a4811a9ea85d14313d8a146b71e222334d
Cezar Halmagean <cezar@mixandgo.com>
2022-06-22 08:38:34 +0300

    Added posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(created_at: :desc).all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    current_user.posts << @post
+    @user = current_user
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:body, :file)
+    end
+end
