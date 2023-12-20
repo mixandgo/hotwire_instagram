79ab20004dea706819efe412c6707ce12fbe2a92
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-12-19 19:41:29 -0500

    Cambiar a PostgreSQL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
+  config.cache_store = :redis_store, ENV["REDIS_URL"], { expires_in: 90.minutes }
+  config.action_cable.url = ENV["REDIS_URL"]
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
