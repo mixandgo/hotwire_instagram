0f1f160007f4480f3217a219ff7728859f6d3f54
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-12-19 19:04:00 -0500

    Cambiar a PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_20_104157) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,8 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_104157) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -50,8 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_104157) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.index ["post_id", "user_id"], name: "index_likes_on_post_id_and_user_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id"
@@ -60,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_104157) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
